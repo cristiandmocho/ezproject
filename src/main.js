9a706c2196c0a2c6887197b30a71b33165edc69c
Cristian Mocho <cristian.mocho@ingka.com>
2022-08-23 09:14:05 +0100

    Adding PrimeVue

--- src/main.js
+++ src/main.js
@@ -5,10 +5,18 @@ import App from './App.vue';
 import router from './router';
 import PrimeVue from 'primevue/config';
 
+// PrimeFaces Styles
+import 'primevue/resources/primevue.min.css'; //core css
+import 'primeicons/primeicons.css';
+
+// Theme
+import 'primevue/resources/themes/saga-blue/theme.css'; //theme
+import '../src/scss/main.scss';
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(PrimeVue);
+app.use(PrimeVue, { ripple: true, inputStyle: 'filled' });
 
 app.mount('#app');
